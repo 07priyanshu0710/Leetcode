c5a2555a05bf0f5ef23e63942e7b1891216ec5ec
prgo-odoo <prgo@odoo.com>
2025-07-23 17:22:19 +0530

    Configure for Render deployment

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . views import leetcode_helper_view
 
 urlpatterns = [
-    path('helper/', leetcode_helper_view, name='helper'),
+    path('', leetcode_helper_view, name='helper'),
 ]
