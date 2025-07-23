265cbbd4f76117f69a6ae775fc0cb29eeb3f1320
prgo-odoo <prgo@odoo.com>
2025-07-23 17:07:48 +0530

    [ADD]leetcode: update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",\
+    "django.contrib.staticfiles",
     "api",
     "corsheaders",
 
@@ -126,9 +126,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-if not DEBUG:
-    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
